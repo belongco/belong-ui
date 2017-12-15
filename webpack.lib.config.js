ff8f3f7992e9e69e79f657c3972f3c8b8b253b97
vivek kaarthek <vivek@viveks-MacBook-Pro.local>
2017-12-15 21:26:53 +0530

    Add Webpack Build Library Step

---
+++ webpack.lib.config.js
@@ -0,0 +1,76 @@
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+
+function config(env) {
+  const packageName = env.pkg;
+  const extractTextPlugin = new ExtractTextPlugin({ filename: `belong-ui.${packageName}.min.css`, allChunks: true });
+
+  return {
+    entry: path.resolve(__dirname, `./packages/${packageName}/index.js`),
+    output: {
+      path: path.resolve(__dirname, `./packages/${packageName}/dist/`),
+      filename: `belong-ui.${packageName}.min.js`,
+      library: `belongUI${packageName}`,
+      libraryTarget: 'umd',
+    },
+    externals: {
+      lodash: 'lodash',
+      classnames: 'classnames',
+      react: 'react',
+      jquery: 'jquery',
+      'prop-types': 'prop-types',
+      'react-dom': 'react-dom',
+    },
+    resolveLoader: {
+      moduleExtensions: ['-loader'],
+    },
+    module: {
+      rules: [{
+        test: /\.jsx?$/,
+        exclude: /(node_modules|.\/flash\/frontend\/js\/libs\/)/,
+        loader: 'babel',
+        query: {
+          plugins: [
+            'transform-runtime',
+            'transform-react-remove-prop-types',
+            'add-react-displayname',
+          ],
+          presets: [['es2015', { modules: false }], 'stage-0', 'react'],
+        },
+      }, {
+        test: /\.scss$/,
+        exclude: /(node_modules|bower_components)/,
+        use: extractTextPlugin.extract({
+          fallback: 'style',
+          use: [
+            {
+              loader: 'css?minimize',
+            },
+            {
+              loader: 'postcss',
+              options: {
+                plugins: [autoprefixer({ browsers: ['> 1%', 'last 2 versions'] })],
+              },
+            },
+            {
+              loader: 'sass',
+              options: {
+                outputStyle: 'compressed',
+              },
+            },
+          ],
+        }),
+      }],
+      noParse: /node_modules\/quill\/dist/,
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({ minimize: true, sourceMap: false, compress: { warnings: false, drop_console: true } }),
+      extractTextPlugin,
+    ],
+  };
+}
+
+module.exports = config;
